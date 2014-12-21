4edb6176927b375e08a3cd9248c93739d40694f7
aminalhazwani <amin.alhazwani@gmail.com>
2014-12-21 23:02:27 +0100

    Progress on the index page

--- site/templates/index.php
+++ site/templates/index.php
@@ -1,31 +1,29 @@
 <?php snippet('header') ?>
 
 	<main class="index" role="main">
-		<nav role="navigation">
-		<ul>
-			<?php foreach($pages->find('articles')->children()->visible() as $article): ?>
-			<li class="scene_element scene_element--fadeinup">
-		  		<a href="<?php echo $article->url() ?>" style="background-image:url(<?php foreach($article->images() as $image): ?><?php if($image->cover() != ''): ?><?php echo $image->url() ?><?php endif ?><?php endforeach?>)">
-		  			<div class="article__header--overlay"></div>
-		  			<div class="article__header--info">
-			  			<div class="article__header--meta">
-					        <h1><?php echo $article->title()->html() ?></h1>
-					        <div>
-					            <span>Article by</span>
-					            <h3><a href="mailto:<?php echo $article->authormail()->html() ?>"><?php echo $article->author()->html() ?></a></h3>
-					            <p><?php echo $article->authoraffilation()->html() ?></p>
+		<nav class="index__navation" role="navigation">
+			<ul class="index__navigation--list">
+				<?php foreach($pages->find('articles')->children()->visible() as $article): ?>
+				<li class="scene_element scene_element--fadeinup index__item">
+			  		<a href="<?php echo $article->url() ?>" style="background-image:url(<?php foreach($article->images() as $image): ?><?php if($image->cover() != ''): ?><?php echo $image->url() ?><?php endif ?><?php endforeach?>)">
+			  			<div class="article__header--overlay"></div>
+			  			<div class="index__item--info">
+				  			<div class="index__item--meta">
+						        <h1 class="index__item--title"><?php echo $article->title()->html() ?></h1>
+						        <div class="index__item--authors">
+						            by <strong><?php echo $article->author()->html() ?></strong>
+						            with <strong><?php echo $article->designer()->html() ?></strong>
+						        </div>
+	        				</div>
+
+	        				<div class="index__item--description">
+					            <?php echo $article->description()->kirbytext() ?>
 					        </div>
-					        <div>
-					            <span>Design by</span>
-					            <h3><a href="mailto:<?php echo $article->designeremail()->html() ?>"><?php echo $article->designer()->html() ?></a></h3>
-					            <p><?php echo $article->designeraffilation()->html() ?></p>
-					        </div>
-        				</div>
-        			</div>
-		  		</a>
-			</li>
-			<?php endforeach ?>
-		</ul>
+					    </div>
+			  		</a>
+				</li>
+				<?php endforeach ?>
+			</ul>
 		</nav>
 	</main>
 
