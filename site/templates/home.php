e61237f362ad4a5f79acac9f4c28eeea4aef11ce
aminalhazwani <amin.alhazwani@gmail.com>
2015-01-10 00:42:16 +0100

    Oprimized upper side of homepage

--- site/templates/home.php
+++ site/templates/home.php
@@ -12,6 +12,7 @@
 	</section>
 
 	<div class="home__cover">
+		<p>The Publication</p>
 		<img src="/assets/images/dod-lettering.png" />
 	</div>
 
@@ -22,13 +23,26 @@
   	</section>
 
   	<section>
-	  	<p>
-	  		Nulla vitae elit libero, a pharetra augue. Maecenas faucibus mollis interdum. Etiam porta sem malesuada magna mollis euismod. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-	  	</p>
-	  	<a href="#">Index</a>
-	  	<a href="#">epub</a>
-	  	<a href="#">kindle</a>
-	  	<a href="#">pdf</a>
+  		<div>
+	  		<p>
+		  		Nulla vitae elit libero, a pharetra augue. Maecenas faucibus mollis interdum. Etiam porta sem malesuada magna mollis euismod. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+		  	</p>
+  		</div>
+	  	<a href="#">Read online</a>
+
+	  	or
+
+	  	<p>for iphone, ipad, and nook</p>
+	  	<a href="#">download epub</a>
+
+	  	<p>for kindle (or kindle app on ipad)</p>
+	  	<a href="#">download mobi</a>
+
+	  	<p>other devices</p>
+	  	<a href="#">download pdf</a>
+
+	  	<p>all files for all devices</p>
+	  	<a href="#">download zip</a>
   	</section>
   </main>
 
