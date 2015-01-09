5c24fc2fd7d25b6051f37a171deb543dcdd82f75
aminalhazwani <amin.alhazwani@gmail.com>
2015-01-10 00:10:36 +0100

    Started to style homepage

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,7 +1,35 @@
 <?php snippet('header') ?>
 
-  <main class="main" role="main">
+  <main class="home" role="main">
+	<section class="home__header">
+		<div>
+	  		<p>Proceedings of the conference By Design or By Disaster 2013</p>
+	  	</div>
+	  	<div>
+		  	<a href="#">Read</a>
+		  	<a href="#">Download</a>
+		</div>
+	</section>
 
+	<div class="home__cover">
+		<img src="/assets/images/dod-lettering.png" />
+	</div>
+
+  	<section>
+  		<a href="#">Facebook</a>
+  		<a href="#">Twitter</a>
+  		<a href="#">E-Mail</a>
+  	</section>
+
+  	<section>
+	  	<p>
+	  		Nulla vitae elit libero, a pharetra augue. Maecenas faucibus mollis interdum. Etiam porta sem malesuada magna mollis euismod. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+	  	</p>
+	  	<a href="#">Index</a>
+	  	<a href="#">epub</a>
+	  	<a href="#">kindle</a>
+	  	<a href="#">pdf</a>
+  	</section>
   </main>
 
 <?php snippet('footer') ?>
