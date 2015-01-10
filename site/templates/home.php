fd1805026f75356a0d0370ae7becd2a468dc6228
aminalhazwani <amin.alhazwani@gmail.com>
2015-01-10 10:28:51 +0100

    Progress on the homepage

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,7 +1,7 @@
 <?php snippet('header') ?>
 
   <main class="home" role="main">
-	<section class="home__header">
+	<section class="home__header scene_element scene_element--fadeinup">
 		<div>
 	  		<p>Proceedings of the conference By Design or By Disaster 2013</p>
 	  	</div>
@@ -11,18 +11,18 @@
 		</div>
 	</section>
 
-	<div class="home__cover">
+	<div class="home__cover scene_element scene_element--fadeinup">
 		<p>The Publication</p>
 		<img src="/assets/images/dod-lettering.png" />
 	</div>
 
-  	<section>
+  	<section class="scene_element scene_element--fadeinup"> 
   		<a href="#">Facebook</a>
   		<a href="#">Twitter</a>
   		<a href="#">E-Mail</a>
   	</section>
 
-  	<section>
+  	<section class="scene_element scene_element--fadeinup">
   		<div>
 	  		<p>
 		  		Nulla vitae elit libero, a pharetra augue. Maecenas faucibus mollis interdum. Etiam porta sem malesuada magna mollis euismod. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
