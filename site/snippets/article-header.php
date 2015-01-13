f3801ac8a11356e72b9927b39b2a1e722e4b48b5
aminalhazwani <amin.alhazwani@gmail.com>
2015-01-13 19:10:04 +0100

    Optimized nav btm style and article header layout

--- site/snippets/article-header.php
+++ site/snippets/article-header.php
@@ -2,7 +2,7 @@
     <div class="article__header--info">
         <?php snippet('article-top-navigation') ?>
         <div class="article__header--meta">
-            <h1 class="article__header--title"><?php echo $page->title()->html() ?></h1>
+            <h1 class="article__header--title article__header--title--left"><?php echo $page->title()->html() ?></h1>
             <div class="article__header--author">
                 <span>Article by</span>
                 <h3 class="article__header--author--name"><a href="mailto:<?php echo $page->authormail()->html() ?>"><?php echo $page->author()->html() ?></a></h3>
@@ -18,6 +18,7 @@
         </div>
 
         <div class="article__header--abstract">
+            <h1 class="article__header--title article__header--title--right"><?php echo $page->title()->html() ?></h1>
             <?php echo $page->description()->kirbytext() ?>
         </div>
     </div>
