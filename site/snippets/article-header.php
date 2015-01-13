5af2e8535213345277346ba06435a30e5d0daf22
aminalhazwani <amin.alhazwani@gmail.com>
2015-01-13 23:05:08 +0100

    Smaller titles if they are long (check in future)

--- site/snippets/article-header.php
+++ site/snippets/article-header.php
@@ -2,7 +2,8 @@
     <div class="article__header--info">
         <?php snippet('article-top-navigation') ?>
         <div class="article__header--meta">
-            <h1 class="article__header--title article__header--title--left"><?php echo $page->title()->html() ?></h1>
+            <h1 class="article__header--title article__header--title--left <?php if(strlen($page->title()) > 24): ?>article__header--title--long<?php endif ?>">
+            <?php echo $page->title()->html() ?></h1>
             <div class="article__header--author">
                 <span>Article by</span>
                 <h3 class="article__header--author--name"><a href="mailto:<?php echo $page->authormail()->html() ?>"><?php echo $page->author()->html() ?></a></h3>
@@ -18,7 +19,7 @@
         </div>
 
         <div class="article__header--abstract">
-            <h1 class="article__header--title article__header--title--right"><?php echo $page->title()->html() ?></h1>
+            <h1 class="article__header--title article__header--title--right <?php if(strlen($page->title()) > 24): ?>article__header--title--long<?php endif ?>"><?php echo $page->title()->html() ?></h1>
             <?php echo $page->description()->kirbytext() ?>
         </div>
     </div>
