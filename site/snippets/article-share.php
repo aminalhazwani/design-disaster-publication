eba1b28122e7c595ca82c67f9e3610f7449d9a7c
aminalhazwani <amin.alhazwani@gmail.com>
2015-01-15 16:51:24 +0100

    Minor changes to article share

--- site/snippets/article-share.php
+++ site/snippets/article-share.php
@@ -2,7 +2,7 @@
 	<p></p>
 	<div class="article__share--icons">
 		<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $page->url() ?>" class="article__share--fbk"></a>
-		<a href="https://twitter.com/intent/tweet?url=<?php echo $page->url() ?>&text=<?php echo $page->title() ?> by <?php echo $page->author()->html() ?> and <?php echo $page->designer()->html() ?>" class="article__share--bird"></a>
-		<a href="mailto:" class="article__share--mail"></a>
+		<a href="https://twitter.com/intent/tweet?url=<?php echo $page->url() ?>&text=<?php echo $page->title() ?> - <?php echo $site->title()->html() ?>" class="article__share--bird"></a>
+		<a href="mailto:?subject=<?php echo $page->title() ?> - <?php echo $site->title()->html() ?>&body=<?php echo $page->url() ?>" class="article__share--mail"></a>
 	</div>
 </section>
