35764143d67b6360da295db22a6a9f401e485ad2
aminalhazwani <amin.alhazwani@gmail.com>
2015-04-03 17:51:19 +0200

    Removed smoothstate js

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -70,7 +70,7 @@
     <!-- Google analytics script -->
     <?php snippet('analyticstracking') ?>
 </head>
-<body id="main" class="m-scene">
+<body>
     <!--[if lt IE 7]>
         <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
