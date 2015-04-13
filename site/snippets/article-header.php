103f2c7c5ff3b0fafa0f681f0bc3cad72a7235d5
aminalhazwani <amin.alhazwani@gmail.com>
2015-04-13 14:48:44 +0200

    Hyphenation for article header title

--- site/snippets/article-header.php
+++ site/snippets/article-header.php
@@ -2,7 +2,7 @@
     <div class="article__header--info">
         <?php snippet('article-top-navigation') ?>
         <div class="article__header--meta">
-            <h1 class="article__header--title<?php if(strlen($page->title()) > 24): ?> article__header--title--long<?php endif ?>">
+            <h1 class="article__header--title<?php if(strlen($page->title()) > 20): ?> article__header--title--long<?php endif ?>">
             <?php echo $page->title()->html() ?></h1>
             <div class="article__header--author">
                 <span>Article by</span>
