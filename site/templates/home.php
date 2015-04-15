2b9482e04913136f695943dcd448ab09350c00d8
aminalhazwani <amin.alhazwani@gmail.com>
2015-04-15 17:10:19 +0200

    Added draft images

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,36 +1,32 @@
 <?php snippet('header') ?>
 
-  <main class="home" role="main">
-	<!-- <section class="home__header">
-		<?php echo $page->introtext()->kirbytext() ?>
-	</section> -->
-
-	<div class="home__cover">
-		<div>
-			<?php echo $page->covertext()->kirbytext() ?>
-		</div>
-		<img src="assets/images/dod-lettering.png" />
-	</div>
-
-  	<section class="home__read">
-  		<div class="home__read--online">
-  			<div>
-	  			<h3><?php echo $page->onlinetitle() ?></h3>
-	  			<img class="home__read--img-mobile" src="assets/images/read-placeholder.png" />
-		  		<?php echo $page->onlinetext()->kirbytext() ?>
+	<main class="home" role="main">
+		<div class="home__hero">
+			<div class="home__hero--title">
+				<?php echo $page->covertext()->kirbytext() ?>
 			</div>
-			<img class="home__read--online--image" src="assets/images/read-placeholder.png" />
-  		</div>
+			<img class="home__hero--image" src="assets/images/dod-lettering.png" />
+		</div>
 
-  		<div id="download" class="home__read--offline">
-  			<img class="home__read--offline--image" src="assets/images/read-placeholder.png" />
-  			<div>
-	  			<h3><?php echo $page->offlinetitle() ?></h3>
-	  			<img class="home__read--img-mobile" src="assets/images/read-placeholder.png" />
-	  			<?php echo $page->offlinetext()->kirbytext() ?>
-  			</div>
-  		</div>
-  	</section>
+	  	<section class="home__read">
+	  		<div class="home__read--online">
+	  			<div class="read__wrapper">
+		  			<h3 class="read__wrapper--title"><?php echo $page->onlinetitle() ?></h3>
+		  			<img class="read__wrapper--img-mobile" src="assets/images/online_1200px.png" />
+			  		<div class="read__wrapper--text"><?php echo $page->onlinetext()->kirbytext() ?></div>
+				</div>
+				<img class="read__wrapper--image-online" src="assets/images/online_1200px.png" />
+	  		</div>
+
+	  		<div id="download" class="home__read--offline">
+	  			<img class="read__wrapper--image-offline" src="assets/images/offline_1200px.png" />
+	  			<div class="read__wrapper">
+		  			<h3 class="read__wrapper--title"><?php echo $page->offlinetitle() ?></h3>
+		  			<img class="read__wrapper--img-mobile" src="assets/images/offline_1200px.png" />
+		  			<div class="read__wrapper--text"><?php echo $page->offlinetext()->kirbytext() ?></div>
+	  			</div>
+	  		</div>
+	  	</section>
   </main>
 
 <?php snippet('footer') ?>
