7c1f1270c00d72c5a0be3737632564c2b60bee9c
aminalhazwani <amin.alhazwani@gmail.com>
2015-04-18 19:02:52 +0200

    Added download template and blueprint

---
+++ site/templates/download.php
@@ -0,0 +1,10 @@
+<?php snippet('header') ?>
+
+  <main class="download" role="main">
+  	<section>
+	  	<h1><?php echo $page->title()->html() ?></h1>
+	    <?php echo $page->text()->kirbytext() ?>
+	</section>
+  </main>
+
+<?php snippet('footer') ?>
